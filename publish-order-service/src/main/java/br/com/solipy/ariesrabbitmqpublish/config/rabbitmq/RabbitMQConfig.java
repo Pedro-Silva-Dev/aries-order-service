b6230b974d3530e01e17b7c972d47d4e0612e365
Pedro Silva <pedro.silva.dev.online@gmail.com>
2023-07-08 23:38:32 -0300

    fix startar conexao com o rabbitMq ao iniciar a aplicacao

--- publish-order-service/src/main/java/br/com/solipy/ariesrabbitmqpublish/config/rabbitmq/RabbitMQConfig.java
+++ publish-order-service/src/main/java/br/com/solipy/ariesrabbitmqpublish/config/rabbitmq/RabbitMQConfig.java
@@ -2,34 +2,44 @@ package br.com.solipy.ariesrabbitmqpublish.config.rabbitmq;
 
 import br.com.solipy.ariesrabbitmqpublish.util.RabbitMqQueue;
 import br.com.solipy.ariesrabbitmqpublish.util.RabbitMqRouteKey;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class RabbitMQConfig {
-
     @Value("${rabbitmq.exchange.name}")
     private String exchange;
 
-    @Bean
+    private final AmqpAdmin amqpAdmin;
+
+
     public Queue orderServiceQueue(){
         return new Queue(RabbitMqQueue.ORDER_SERVICE, true, false, false);
     }
 
-    @Bean
     public DirectExchange directExchange() {
         return new DirectExchange(exchange);
     }
 
-    @Bean
     public Binding createOrderServiceBinding(){
         return BindingBuilder
                 .bind(orderServiceQueue())
                 .to(directExchange())
                 .with(RabbitMqRouteKey.ORDER_SERVICE_KEY);
+    }
+
+    @PostConstruct
+    private void addConection(){
+        this.amqpAdmin.declareQueue(orderServiceQueue());
+        this.amqpAdmin.declareExchange(directExchange());
+        this.amqpAdmin.declareBinding(createOrderServiceBinding());
     }
 
 
